fdd98330b59431488196763df6ca26342cdcb918
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-06 20:06:01 +0900

    [Improve] some to strjoin

--- strjoin/test.c
+++ strjoin/test.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "libft.h"
 
 int main()
 {
-	char *s1 = "12345", *s2 = "67890", *str;
+	char *s1 = "where is my ", *s2 = "malloc ???", *str;
 	str = ft_strjoin(s1, s2);
 	printf("%s\n", str);
+
+	char *s3 = "where is my malloc ???";
+	if (!strcmp(str, s3))
+		printf("success\n");
+	else
+		printf("str: %s\ns3: %s\n", str, s3);
 	return (0);
 }
