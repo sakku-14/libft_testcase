188243dba1ae0324b8852c5e3b4d8fde360243cb
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-07 19:51:53 +0900

    [Add] itoa

---
+++ itoa/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	printf("%s\n", ft_itoa(1234));
+	printf("%s\n", ft_itoa(-1234));
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa('c'));
+	return (0);
+}
