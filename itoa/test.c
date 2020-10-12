c8dc58bc3a094190a1d7b3dd5fc542e0de8bc2a1
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-12 21:05:27 +0900

    [Improve] itoa

--- itoa/test.c
+++ itoa/test.c
@@ -7,5 +7,7 @@ int main()
 	printf("%s\n", ft_itoa(-1234));
 	printf("%s\n", ft_itoa(0));
 	printf("%s\n", ft_itoa('c'));
+	printf("%s\n", ft_itoa(-2147483648));
+	printf("%s\n", ft_itoa((-2147483647 -1)));
 	return (0);
 }
